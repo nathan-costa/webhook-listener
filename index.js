c07f5ecbee36fc9df0a60cb021f68cf9d9dc4b19
nathanpower-nearform <nathan@costadigital.io>
2015-08-18 10:08:40 +0100

    [#99903730] Webhook listener - initial commit]

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var execSync = require('exec-sync');
+var yaml_config = require('node-yaml-config');
+var config = yaml_config.load(__dirname + '/config.yml');
+
+app.get(config.webhook.url, function (req, res) {
+    result = execSync(config.command);
+    console.log(result);
+});
+
+var server = app.listen(config.server.port, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
